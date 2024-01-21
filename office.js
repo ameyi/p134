4056471f9e03e30a3dde28301757bf254598aeba
ameyi <114318190+ameyi@users.noreply.github.com>
2024-01-21 10:59:04 -0800

    Add files via upload

---
+++ office.js
@@ -0,0 +1,45 @@
+random = ""
+status1 = ""
+objects = []
+function preload(){
+    random = loadImage("office.jpeg")
+}
+function setup(){
+    canvas = createCanvas(640, 480)
+    canvas.center()
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
+}
+
+function draw(){
+    image(random, 0, 0, 640, 480)
+    if(status1 != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+            fill("#00FFFF")
+            percent = floor(objects[i].confidence * 100)
+            text(objects[i].label+" "+percent+ "%", objects[i].x, objects[i].y)
+            noFill()
+            stroke("#00FFFF")
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
+    
+}
+function modelLoaded(){
+    console.log("Model has been loaded")
+    status1 = true;
+    objectDetector.detect(random, gotResult)
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results)
+        objects = results;
+    }
+}
+function home(){
+    window.location.replace("index.html")
+}
